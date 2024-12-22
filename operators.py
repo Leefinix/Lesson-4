e1f467161e1663f7e94e98a72e9176719ece13bb
Leefinix <joaquinnleo@gmail.com>
2024-12-22 15:06:16 +0700

    operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+a = 67
+b = 28
+c = 94
+d = 85
+e = 15
+
+print(a,"\n")
+print(b,"\n")
+print(c,"\n")
+print(d,"\n")
+print(e,"\n")
+
+sum = a + b + c + d + e
+print("The sum of the values are : ", sum)
+
+average = sum/5
+print("The average of the values are: ", average)
